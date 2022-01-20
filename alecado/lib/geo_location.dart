58ce5bb6fc991de13a99ead08f96569623a6bb15
Aakash Sinha <jshruti1215@gmail.com>
2022-01-21 00:41:01 +0530

    Update geo_location.dart

--- alecado/lib/geo_location.dart
+++ alecado/lib/geo_location.dart
@@ -24,6 +24,11 @@ class _google_mapState extends State<google_map> {
     });
   }
 
+  initState() async {
+    await _onMapCreated;
+    await _getGeoLocationPosition;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,41 +56,6 @@ class geoloc extends StatefulWidget {
 class _geolocState extends State<geoloc> {
   String location = 'Null, Press Button';
   String Address = '';
-
-  Future<Position> _getGeoLocationPosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      await Geolocator.openLocationSettings();
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-  }
 
   Future<void> GetAddressFromLatLong(Position position) async {
     List<Placemark> placemarks =
