cc6bc8e28f95de36e7bdc31a43701ed9b4f95d8b
Aakash Sinha <jshruti1215@gmail.com>
2022-01-21 00:12:02 +0530

    map update

--- alecado/lib/geo_location.dart
+++ alecado/lib/geo_location.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class google_map extends StatefulWidget {
+  const google_map({Key? key}) : super(key: key);
+
+  @override
+  _google_mapState createState() => _google_mapState();
+}
+
+class _google_mapState extends State<google_map> {
+  Set<Marker> _markers = {};
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() async {
+      Position position = await _getGeoLocationPosition();
+      double lat = position.latitude;
+      double lon = position.longitude;
+      _markers.add(Marker(
+        markerId: MarkerId('id-1'),
+        position: LatLng(lat, lon),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GoogleMap(
+      onMapCreated: _onMapCreated,
+      markers: _markers,
+      initialCameraPosition:
+          CameraPosition(target: LatLng(27.5614547, 78.6644839), zoom: 100),
+    ));
+  }
+}
+
+//------------------------------------------------------------------------------
 
 class geoloc extends StatefulWidget {
   const geoloc({Key? key}) : super(key: key);
@@ -138,4 +174,39 @@ class _geolocState extends State<geoloc> {
       ],
     );
   }
+}
+
+Future<Position> _getGeoLocationPosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    await Geolocator.openLocationSettings();
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
 }
