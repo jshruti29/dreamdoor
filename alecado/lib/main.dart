f5f3998777462b3136278386c27994448d4bf8b0
Aakash Sinha <jshruti1215@gmail.com>
2022-01-21 12:57:10 +0530

    test

--- alecado/lib/main.dart
+++ alecado/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:alecado/geo_location.dart';
 import 'package:alecado/map.dart';
 import 'package:flutter/material.dart';
 import 'Vendor/LandingPagesVendor/one_vendor_landing_page.dart';
+import 'new_map.dart';
 
 void main() {
   runApp(const MyAppu());
@@ -19,6 +20,6 @@ class MyAppu extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: setlocation());
+    return MaterialApp(home: MyLocation());
   }
 }
